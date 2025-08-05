bd388b74a36ce7034f4bf148e75dfe10cfe2461c
Thapelo Nobatana <nobatanathapelo1@gmail.com>
2025-08-05 14:48:26 +0200

    feat: set up Context API to share counter state between Header and CounterApp components

--- alx-project-0x05/context/CountContext.tsx
+++ alx-project-0x05/context/CountContext.tsx
@@ -1,5 +1,12 @@
-import { CountContextProps } from "@/interface";
+// import { CountContextProps } from "@/interface";
 import { createContext, useContext, useState, ReactNode } from "react";
+
+
+interface CountContextProps {
+  count: number
+  increment: () => void
+  decrement: () => void
+}
 
 export const CountContext = createContext<CountContextProps | undefined>(undefined);
 
